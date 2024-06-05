688d8c41c5732ad8cda342bb1baec338e2e79e07
Mohamed Amir Bekhouche <mohamed.bekhouche@etu.univ-smb.fr>
2024-06-05 19:30:01 +0200

    iugcfgvcb

---
+++ Dortan/Activite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dortan
+{
+    public class Activite
+    {
+        private string nom;
+
+        public string Nom
+        {
+            get { return this.nom; }
+            set { this.nom = value; }
+        }
+
+        public Activite(string nom)
+        {
+            this.Nom = nom;
+        }
+    }
+}
