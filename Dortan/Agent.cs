d3cc050c8757969101d7145d53f6775c25e3421d
Mohamed Amir Bekhouche <mohamed.bekhouche@etu.univ-smb.fr>
2024-06-05 19:03:17 +0200

    on commence malheuresement sans arytom

---
+++ Dortan/Agent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dortan
+{
+    class Agent
+    {
+		private string identifiant;
+		private string password;
+
+		public string Password
+		{
+			get { return this.password; }
+			set { this.password = value; }
+		}
+
+
+		public string Identifiant
+		{
+			get { return this.identifiant; }
+			set { this.identifiant = value; }
+		}
+
+	}
+}
---
+++ Dortan/ApplicationData.cs
@@ -0,0 +1,64 @@
+﻿
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_BD_Locale
+{
+
+        public class ApplicationData
+        {
+       
+            private ObservableCollection<Agent> lesClients;
+            private NpgsqlConnection connexion = null;   // futur lien à la BD
+
+
+        public ObservableCollection<Client> LesClients
+        {
+            get
+            {
+                return this.lesClients;
+            }
+
+            set
+            {
+                this.lesClients = value;
+            }
+        }
+
+       public NpgsqlConnection Connexion 
+        {
+            get
+            {
+                return this.connexion ;
+            }
+
+            set
+            {
+                this.connexion  = value;
+            }
+        }
+       
+	   public ApplicationData()
+        {
+           
+            this.ConnexionBD();
+            this.Read();
+        }
+        public void ConnexionBD()
+            {
+            
+            }
+        public void Read()
+            {
+             this.LesClients = new ObservableCollection<Client>();
+            }
+        
+        }
+    
+}
