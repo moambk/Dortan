6bdb7c604b33e0b2638406f6fd908673ff78256e
Mohamed Amir Bekhouche <mohamed.bekhouche@etu.univ-smb.fr>
2024-06-06 18:29:22 +0200

    exeption

--- Dortan/Employe.cs
+++ Dortan/Employe.cs
@@ -14,14 +14,24 @@ namespace Dortan
 		public string Password
 		{
 			get { return this.password; }
-			set { this.password = value; }
+			set {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("mot de passe doit etre renseigné");
+                if (value.Length > 20)
+                    throw new ArgumentException("login trop long");
+                this.password = value; }
 		}
 
 
 		public string Identifiant
 		{
 			get { return this.identifiant; }
-			set { this.identifiant = value; }
+			set {
+				if (string.IsNullOrEmpty(value))
+					throw new ArgumentNullException("login doit etre renseigné");
+				if (value.Length > 8)
+					throw new ArgumentException("login trop long");
+				this.identifiant = value; }
 		}
 
         public Employe(string password, string identifiant)
