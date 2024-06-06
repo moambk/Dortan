f83ed1eba5843288f87c77bceb6185df69c08a8d
Mohamed Amir Bekhouche <mohamed.bekhouche@etu.univ-smb.fr>
2024-06-06 02:04:41 +0200

    dataAcces

---
+++ Dortan/DataAccess.cs
@@ -0,0 +1,100 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dortan
+{
+
+        public class DataAccess
+        {
+            private static DataAccess instance;
+            private static string strConnexion = "Server=srv-peda-new;port=5433;"
+                             + "Database=;Search Path=;uid=;password=;";
+            private DataAccess()
+            {
+                ConnexionBD();
+            }
+
+
+            public static DataAccess Instance
+            {
+                get
+                {
+                    if (instance == null)
+                    {
+                        instance = new DataAccess();
+                    }
+                    return instance;
+                }
+            }
+
+            public NpgsqlConnection? Connexion
+            {
+                get;
+                set;
+            }
+            public void ConnexionBD()
+            {
+                try
+                {
+                    Connexion = new NpgsqlConnection();
+                    Connexion.ConnectionString = strConnexion;
+                    Connexion.Open();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("pb de connexion : " + e);
+                    // juste pour le debug : à transformer en MsgBox  
+                }
+            }
+
+
+
+            public void DeconnexionBD()
+            {
+                try
+                {
+                    Connexion.Close();
+                }
+                catch (Exception e)
+                { Console.WriteLine("pb à la déconnexion  : " + e); }
+            }
+
+
+            public DataTable GetData(string selectSQL)
+            {
+                try
+                {
+                    NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(selectSQL, Connexion);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    return dataTable;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("pb avec : " + selectSQL + e.ToString());
+                    return null;
+                }
+            }
+
+            public int SetData(string setSQL)
+            {
+
+                try
+                {
+                    NpgsqlCommand sqlCommand = new NpgsqlCommand(setSQL, Connexion);
+                    int nbLines = sqlCommand.ExecuteNonQuery();
+                    return nbLines;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("pb avec : " + setSQL + e.ToString());
+                    return 0;
+                }
+            }
+        }
+ }
